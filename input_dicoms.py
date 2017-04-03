300a6de2cf9f382de465333f1ee1eaf8077f9404
filareta <filaretayordanova@gmail.com>
2017-04-03 12:34:39 +0300

    More constants, use configurable variables in input_dicoms

--- input_dicoms.py
+++ input_dicoms.py
@@ -7,35 +7,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import compress_dicoms as cd
 import lung_segmentation as ls
-
-
-MIN_BOUND = -1000.0
-MAX_BOUND = 400.0
-PIXEL_MEAN = 0.25
-IMAGE_PXL_SIZE = 512
-# IMAGE_PXL_SIZE = 180
-HM_SLICES = 450
-# HM_SLICES = 277
-THRESHOLD_LOW = -1100.0
-THRESHOLD_HIGH = 700.0
+import config
 
 
 def resize(image):
     if not len(image):
         return np.array([])
 
-    return np.stack([cv2.resize(scan, (IMAGE_PXL_SIZE, IMAGE_PXL_SIZE)) for scan in image])
+    return np.stack([cv2.resize(scan, (config.IMAGE_PXL_SIZE_X, config.IMAGE_PXL_SIZE_Y)) 
+                     for scan in image])
 
 
 def normalize(image):
-    image = (image - MIN_BOUND) / (MAX_BOUND - MIN_BOUND)
+    image = ((image - config.MIN_BOUND) / 
+             (config.MAX_BOUND - config.MIN_BOUND))
     image[image>1] = 1.
     image[image<0] = 0.
-    return image
-
-
-def zero_center(image):
-    image = image - PIXEL_MEAN
     return image
 
 
@@ -48,20 +35,19 @@ def chunks(l, n):
 def mean(l):
     if len(l):
         return sum(l) / len(l)
-    return np.full(l.shape, MIN_BOUND, l.dtype)
+    return np.full(l.shape, config.OUT_SCAN, l.dtype)
 
 
 def get_mean_chunk_slices(slices):
-    if len(slices) < HM_SLICES:
+    if len(slices) < config.SLICES:
         print("New slices are less then required after getting mean images, adding padding.")
-        return trim_and_pad(np.array(slices), HM_SLICES) 
+        return trim_and_pad(np.array(slices), config.SLICES) 
 
     new_slices = []
-    for slice_chunk in np.array_split(slices, HM_SLICES):
+    for slice_chunk in np.array_split(slices, config.SLICES):
         slice_chunk = list(map(mean, zip(*slice_chunk)))
         new_slices.append(slice_chunk)
 
-    # print(len(slices), len(new_slices))
     return np.stack(new_slices)
 
 
@@ -73,20 +59,6 @@ def read_csv_column(input_file, columns=[0]):
     return pd.read_csv(input_file, usecols=columns).values.flatten()
 
 
-def threshold_and_normalize_scan(scan):
-    scan = scan.astype(np.float32)
-    scan [scan < THRESHOLD_LOW] = THRESHOLD_LOW
-    scan [scan > THRESHOLD_HIGH] = THRESHOLD_HIGH
-    
-    # Maximum absolute value of any pixel .
-    max_abs = abs (max(THRESHOLD_LOW, THRESHOLD_HIGH, key=abs))
-    
-    # This will bring values between -1 and 1
-    scan /= max_abs
-    
-    return scan
-
-
 def trim_and_pad(patient_img, slice_count, normalize_pad=True):
     slices, size_x, size_y = patient_img.shape
 
@@ -96,20 +68,21 @@ def trim_and_pad(patient_img, slice_count, normalize_pad=True):
     if slices > slice_count:
         return patient_img[:slice_count]
 
-    padding = np.full((slice_count-slices, size_x, size_y), MIN_BOUND, patient_img.dtype)
+    padding = np.full((slice_count-slices, size_x, size_y), 
+        config.OUT_SCAN, patient_img.dtype)
     if normalize_pad:
         padding = normalize(padding)
 
     return np.vstack([patient_img, padding])
 
 
-def get_average_shape(dir=cd.COMPRESSED_DICOMS):
+def get_average_shape(directory=config.SEGMENTED_LUNGS_DIR):
     slice_counts = 0
     img_size = 0
     total = 0
     max_slices = 0
-    for patient_id in os.listdir(cd.COMPRESSED_DICOMS):
-        image = cd.load_patient_image(cd.COMPRESSED_DICOMS, patient_id)
+    for patient_id in os.listdir(directory):
+        image = cd.load_patient_image(directory, patient_id)
         nodules = ls.get_lung_nodules_candidates(image)
         slices, size, size = nodules.shape
         if slices > max_slices:
@@ -122,38 +95,8 @@ def get_average_shape(dir=cd.COMPRESSED_DICOMS):
     # Max slices:  389
     # Average shape:  (277, 338, 338)
 
-    # Ony segmentation
+    # Only segmentation
     # Max slices:  533
     # Average shape:  (170, 512, 512)
     print("Max slices: ", max_slices)
     return (int(slice_counts/total), int(img_size/total), int(img_size/total))
-
-
-if __name__ == '__main__':
-    print ("Average shape: ", get_average_shape())
-    for patient_id in os.listdir(cd.COMPRESSED_DICOMS):
-        # try:
-        # patient_id = '00cba091fa4ad62cc3200a657aeb957e'
-        image = cd.load_patient_image(cd.COMPRESSED_DICOMS, patient_id)
-    # image = resize(image)
-
-    # image = trim_and_pad(image, HM_SLICES, False)
-    # image = normalize(image)
-    # image = zero_center(image)
-    # image = get_mean_chunk_slices(image)
-        print(image.shape)
-        scan = image[78]
-        print(scan.shape)
-        print("Segmented: ", scan)
-        plt.imshow(scan, cmap=plt.cm.gray)
-        plt.show()
-    
-        scan[scan < -420] = 0
-        print("Thresholded: ", scan)
-        plt.imshow(scan, cmap=plt.cm.gray)
-        plt.show()
-    # cd.store_patient_image('../kaggle-data/segmented-resized', 
-        # image, patient_id)
-            # print("Patient with id {} has image with shape: {}.".format(patient_id, image.shape))
-        # except Exception as e:
-            # print("Processing {} failed with {}.".format(patient_id, e))
