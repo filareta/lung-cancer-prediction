edb9231409feee90938e865c4e49e63f4a0d7d49
filareta <filaretayordanova@gmail.com>
2017-06-11 16:37:11 +0300

    Add configs without regularization and with various tested dropout options

--- model_configuration.py
+++ model_configuration.py
@@ -38,6 +38,62 @@ class ModelConfig(object):
         return False
 
 
+class BaselineConfig(ModelConfig):
+    from model_definition.baseline import baseline_config
+
+    def __init__(self, conv_layers_num=3, fc_layers_num=2, 
+                 config_dict=baseline_config):
+        super(BaselineConfig, self).__init__(conv_layers_num,
+                                             fc_layers_num,
+                                             config_dict)
+
+
+class NoRegularizationConfig(ModelConfig):
+    from model_definition.additional_layers import additional_layers_config
+
+    def __init__(self, conv_layers_num=4, fc_layers_num=3, 
+                 config_dict=additional_layers_config):
+        super(NoRegularizationConfig, self).__init__(
+            conv_layers_num, fc_layers_num, config_dict)
+
+
+# Three different regularization options used for
+# building the network configuration
+class OneDropoutRegularizationConfig(NoRegularizationConfig):
+    def __init__(self, conv_layers_num, 
+                 fc_layers_num, 
+                 config_dict):
+        super(OneDropoutRegularizationConfig, self).__init__(
+            conv_layers_num, fc_layers_num, config_dict)
+        # Dropout on second fully connected layers, zero based indexing
+        self._fc_layers_with_dropout = [1]
+
+
+class DropoutAfterConvolutionsConfig(OneDropoutRegularizationConfig):
+    def __init__(self, conv_layers_num,
+                 fc_layers_num, 
+                 config_dict):
+        super(DropoutAfterConvolutionsConfig, self).__init__(
+            conv_layers_num, fc_layers_num, config_dict)
+
+    def has_dropout_after_convolutions(self):
+        return True
+
+
+class DropoutsWithL2RegularizationConfig(DropoutAfterConvolutionsConfig):
+    def __init__(self, conv_layers_num,
+                 fc_layers_num, 
+                 config_dict):
+        super(DropoutsWithL2RegularizationConfig, self).__init__(
+            conv_layers_num, fc_layers_num, config_dict)
+
+    def with_l2_norm(self):
+        return True
+
+
+# With regularization - 2 dropouts and L2 norm
+# filters and strides adopted to handle more slices with the same
+# network depth
 class DefaultConfig(ModelConfig):
     from model_definition.default import default_config
 
@@ -53,13 +109,3 @@ class DefaultConfig(ModelConfig):
 
     def with_l2_norm(self):
         return True
-        
-
-class BaselineConfig(ModelConfig):
-    from model_definition.baseline import baseline_config
-
-    def __init__(self, conv_layers_num=3, fc_layers_num=2, 
-                 config_dict=baseline_config):
-        super(BaselineConfig, self).__init__(conv_layers_num,
-                                             fc_layers_num,
-                                             config_dict)
