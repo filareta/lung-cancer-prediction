6dcd077920bbc81b7feeeb1e1c5d9c7d677af9e6
filareta <filaretayordanova@gmail.com>
2017-06-26 00:13:33 +0300

    Use seingle threaded client, because of sparodic ssl failures in the multithreaded version

--- fetch_data/data_client.py
+++ fetch_data/data_client.py
@@ -1,19 +1,12 @@
 import os
-import httplib2
-import multiprocessing
-import concurrent.futures
-import numpy as np
 
 from google.cloud import storage
-from google.cloud.credentials import get_credentials
-
 import googleapiclient.discovery
 
 from oauth2client.client import GoogleCredentials
 
 
 credentials = GoogleCredentials.get_application_default()
-NUM_PROCESSES = multiprocessing.cpu_count()
 
 
 def create_service():
@@ -44,13 +37,13 @@ def list_bucket(bucket):
     return all_objects
 
 
-def collect_blobs_chunk(blobs, project_name, bucket_name, working_dir):
-    http = httplib2.Http()
-    client = storage.Client(project=project_name, 
-        credentials=get_credentials(), _http=http)
+def collect_images(bucket_name, project_name, working_dir='./'):
+    all_blobs = map(lambda item: item['name'], list_bucket(bucket_name))
+
+    client = storage.Client(project=project_name)
     bucket = client.get_bucket(bucket_name)
 
-    for blob_item in blobs:
+    for blob_item in all_blobs:
         blob = storage.Blob(blob_item, bucket)
         complete_path = os.path.join(working_dir, blob_item)
         dir_name = os.path.dirname(complete_path)
@@ -63,31 +56,3 @@ def collect_blobs_chunk(blobs, project_name, bucket_name, working_dir):
                 print("Stored blob path: ", complete_path)
             except Exception as e:
                 print("Downloading {} failed with {}.".format(complete_path, e))
-
-
-def collect_images(bucket_name, project_name, working_dir='./'):
-    all_blobs = [item['name'] for item in list_bucket(bucket_name)]
-    
-    chunked_data = np.array_split(all_blobs, NUM_PROCESSES)
-    print("Number of processes: {}, total chunks of data {}!".format(
-        NUM_PROCESSES, len(chunked_data)))
-
-    executor = concurrent.futures.ThreadPoolExecutor(max_workers=NUM_PROCESSES)
-
-    futures = []
-    for i, blob_chunk in enumerate(chunked_data):
-        try:
-            f = executor.submit(collect_blobs_chunk,
-                                blob_chunk,
-                                project_name,
-                                bucket_name, 
-                                working_dir)
-            print("Submit {} batch to executor!".format(i))
-            futures.append(f)
-        except Exception as e:
-            print("An error occured while downloading blobs chunk in feature {}: {}".format(
-                str(i), e))
-
-    print(concurrent.futures.wait(futures)) # By defaults waits for all
-    print("Shutdown and wait for processes!")
-    executor.shutdown(wait=True)
